3ca3ffb996a821d09bb1d2cde74c1f8623b081fd
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-10-15 20:34:52 -0400

    Add basic test case.

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Spork\Reminders\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as TestbenchTestCase;
+use Spork\Reminders\ReminderServiceProvider.php;
+
+class TestCase extends TestbenchTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Spork\\Reminders\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ReminderServiceProvider.php::class,
+        ];
+    }
+}
+
